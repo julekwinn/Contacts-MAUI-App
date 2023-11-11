0068a779ce97f3ebd425fe01a2b89990b7b4c43d
Juliusz <julek.winn@gmail.com>
2023-11-11 23:09:16 +0100

    Add data Binding to Contact ListView

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace Contacts.Maui.Views;
 
 public partial class ContactsPage : ContentPage
@@ -5,15 +8,33 @@ public partial class ContactsPage : ContentPage
 	public ContactsPage()
 	{
 		InitializeComponent();
-	}
 
-    private void btnAddContact_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
+		List<Contact> contacts= new List<Contact>()
+		{
+			new Contact() {Name="Piotr Kowalski", Email="Piotrk124@wp.pl"},
+
+            new Contact() {Name="Jan Nowaczek", Email="Nowczkiewicz@wp.pl"},
+
+            new Contact() {Name="Justyna Kowalczyk", Email="Justynka_13@wp.pl"},
+
+            new Contact() {Name="Mariusz Pudzianowski", Email="Mariusz_pudzian@gmail.com"},
+
+            new Contact() {Name="Edyta Górniak", Email="edytkadytka@wp.pl"},
+        };
+
+		ListContacts.ItemsSource=contacts;
+
+        BindingContext = this;
     }
 
-    private void btnEditContact_Clicked(object sender, EventArgs e)
+    public class Contact
     {
-        Shell.Current.GoToAsync(nameof(EditContactPage));
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
     }
+
+
 }
