3f71b02b7e4c72166ee3e86a86971badf20ddd2c
Juliusz <julek.winn@gmail.com>
2023-11-11 22:36:16 +0100

    Add buttons to add and edit contacts

--- Contacts.Maui/App.xaml.cs
+++ Contacts.Maui/App.xaml.cs
@@ -10,11 +10,6 @@ namespace Contacts.Maui
 
             MainPage = new AppShell();
 
-            Routing.RegisterRoute(nameof(ContactsPage), typeof(ContactsPage));
-            Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));
-            Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
-
-
         }
     }
 }
